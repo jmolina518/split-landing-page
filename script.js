b5e71371bf5368e33d21cc8a2da788887da811ec
jmolina518 <jmolina518@gmail.com>
2021-02-20 19:55:10 -0600

    add event listener to the right panel

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ left.addEventListener('mouseenter', () => container.classList.add('hover-left'))
 left.addEventListener('mouseleave', () =>
   container.classList.remove('hover-left')
 )
+
+right.addEventListener('mouseenter', () =>
+  container.classList.add('hover-right')
+)
+right.addEventListener('mouseleave', () =>
+  container.classList.remove('hover-right')
+)
